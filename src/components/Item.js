a65b406fb2f10faf749f2be9bfc3ee9c51a3bd18
Brian <mahuibrian14@gmail.com>
2023-04-24 14:00:44 +0300

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React , {useState}from "react";
+/*In the Item component, when the user clicks the <button> element, the item should be added to their virtual cart.
+ The way we'll show the user that the item is in the cart is by changing the className on the <li> element:
+
+<li className="in-cart">
+{/* the item is in the cart *}
+</li>
+<li className="">
+{/* the item is NOT in the cart }
+</li>
+If the item is not in the cart, the <button> 
+element's text should read "Add to Cart", and if the item is in the cart, the <button> element's text should read "Remove From Cart". 
+Naturally, you'll also need to add state to the Item component to solve this deliverable! */
+
+function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+
+function handleAddButton() {
+  setInCart(!inCart); 
+}
+
+return (
+  <li className={inCart ? "in-cart" : ""}>
+    <span>{name}</span>
+    <span className="category">{category}</span>
+    <button onClick={handleAddButton} className="add">
+      {inCart ? "Remove From Cart" : "Add to Cart"}
+    </button>
+  </li>
+);
+}
 
 function Item({ name, category }) {
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddButton} className="add">Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
+/*In the ShoppingList component, there is a <select> element that will allow us to filter the list of items by category.
+
+Use the useState hook to create a state variable called selectedCategory for keeping track of the selected value from 
+this <select> element. When the value of the <select> element is changed, update state.
+
+You should also use the selectedCategory variable to determine which items to display in the shopping list. 
+You'll need some way of filtering the array of items based on the selectedCategory.*/
+
+
+
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+      /*<select name="filter" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>*/</select>
+
+        <select name="filter"
+        onChange={(e) => setSelectedCategory(e.target.value)}
+        value={selectedCategory}
+        >
+           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
+
         </select>
       </div>
+      
+      
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
+  {items
+    .filter((item) => selectedCategory === "All" || item.category === selectedCategory)
+    .map((item) => (
+      <Item key={item.id} name={item.name} category={item.category} />
+    ))}
+</ul>
+</div>
   );
 }
 
 export default ShoppingList;
+
+
